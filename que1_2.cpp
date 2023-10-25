b2f2c15dba1e59fb1723cbef563259313a6fccba
pratik <bhurepratik2@gmail.com>
2023-10-26 00:48:36 +0530

    addition program

---
+++ que1_2.cpp
@@ -0,0 +1,12 @@
+//Write a program to adddition of two numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int no1,no2,sum=0;
+    cout<<"enter no1,no2::";
+    cin>>no1>>no2;
+    sum=no1+no2;
+    cout<<"sum="<<sum;
+}
